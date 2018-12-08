ecfbf05435ed1d51a3970092599320b480f1f159
rotem friedman <rotem.fri@campus.technion.ac.il>
2018-12-08 18:10:10 +0200

    jj

---
+++ love.cpp
@@ -0,0 +1,4 @@
+//
+// Created by rotem on 08/12/2018.
+//
+
---
+++ love.h
@@ -0,0 +1,8 @@
+//
+// Created by rotem on 08/12/2018.
+//
+
+#ifndef HW2_PART2_NOW_LOVE_H
+#define HW2_PART2_NOW_LOVE_H
+
+#endif //HW2_PART2_NOW_LOVE_H
